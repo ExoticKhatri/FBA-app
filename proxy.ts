bc9e360925ab0eb6664e5af2a1fb74cdc09a6184
prakash <prakashgour453@gmail.com>
2026-06-14 12:55:35 +0530

    fixed deployment issue

--- proxy.ts
+++ proxy.ts
@@ -5,6 +5,8 @@ export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
 
+//just pushing for redeploy 
+
 export const config = {
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
